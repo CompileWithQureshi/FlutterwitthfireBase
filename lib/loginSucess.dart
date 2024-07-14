a19938067a8c45dc4083c8e9078a175b7e78185f
MOHAMMED SAJID QURESHI <contactme.qureshi@gmail.com>
2024-07-14 08:33:05 +0530

    Added emoji model file

--- lib/loginSucess.dart
+++ lib/loginSucess.dart
@@ -2,27 +2,35 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-// import 'package:login_pages/util/Emoji.dart';
+import 'package:login_pages/models/emojiScreen.dart';
 import 'package:login_pages/util/mainColumn.dart';
 import 'package:login_pages/util/Text_Color.dart';
 
-class Loginsucess extends StatelessWidget {
+class Loginsucess extends StatefulWidget {
   Loginsucess({super.key});
-  final List emoji = [
-    '👋',
-    '😊',
-    '😂',
-    '🤣',
-    '❤️',
-    '😍',
-    '😒',
-  ];
+
+  @override
+  State<Loginsucess> createState() => _LoginsucessState();
+}
+
+class _LoginsucessState extends State<Loginsucess> {
+  List<EmojiModel> emojis = [];
+
+  void _getCatogry() {
+    emojis = EmojiModel.getCatogry();
+  }
+
+  @override
+  void initState() {
+    _getCatogry();
+  }
 
   @override
   Widget build(BuildContext context) {
+    _getCatogry();
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Secound screens'),
+        title: Text('Secound screens'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
@@ -46,45 +54,7 @@ class Loginsucess extends StatelessWidget {
               SizedBox(
                 width: 50,
               ),
-              Column(
-                children: <Widget>[
-                  Text(
-                    'ListView.Builder',
-                    style: textStyle,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    height: 500,
-                    width: 100,
-                    child: ListView.builder(
-                      itemCount: emoji.length,
-                      itemBuilder: (context, index) {
-                        return InkWell(
-                          onTap: () {
-                            print(emoji[index]);
-                          },
-                          child: Container(
-                            margin: EdgeInsets.symmetric(vertical: 5),
-                            width: 50,
-                            height: 50,
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.black, width: 2),
-                            ),
-                            child: Center(
-                              child: Text(
-                                emoji[index],
-                                style: TextStyle(fontSize: 30),
-                              ),
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              )
+              emojiColumn()
             ],
           )
         ],
@@ -92,33 +62,59 @@ class Loginsucess extends StatelessWidget {
     );
   }
 
-  Container emojiContainer() {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      padding: EdgeInsets.all(5),
-      decoration: BoxDecoration(
-        color: Colors.amber,
-        border: Border.all(
-          color: Colors.pink,
+  Column emojiColumn() {
+    return Column(
+      children: <Widget>[
+        Text(
+          'ListView.Builder',
+          style: textStyle,
         ),
-      ),
+        SizedBox(
+          height: 20,
+        ),
+        Container(
+          height: 500,
+          width: 100,
+          child: ListView.builder(
+            itemCount: emojis.length,
+            itemBuilder: (context, index) {
+              return InkWell(
+                onTap: () {
+                  print(emojis[index].emoji);
+                },
+                child: Container(
+                  margin: EdgeInsets.symmetric(vertical: 15),
+                  width: 50,
+                  height: 100,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 2),
+                      color: emojis[index].boxcolor),
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Text(
+                          emojis[index].emoji,
+                          style: TextStyle(fontSize: 30),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          emojis[index].name,
+                          style: textStyle,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
-
-//  Center(
-//             child: SizedBox(
-//               width: 300,
-//               height: double.infinity,
-//               child: ListView.builder(
-//                 itemBuilder: (context, index) {
-//                   return Emoji(
-//                     child: emoji[index],
-//                   );
-//                 },
-//                 itemCount: emoji.length,
-//               ),
-//             ),
 
 Row MainRow() {
   return Row(
--- lib/models/emojiScreen.dart
+++ lib/models/emojiScreen.dart
@@ -1,1 +1,34 @@
+import 'package:flutter/material.dart';
 
+class EmojiModel {
+  String emoji;
+  String name;
+  Color boxcolor;
+
+  EmojiModel({required this.emoji, required this.boxcolor, required this.name});
+
+  static List<EmojiModel> getCatogry() {
+    List<EmojiModel> emojis = [];
+
+    emojis.add(
+      EmojiModel(emoji: '👋', boxcolor: Color(0xacfa70), name: 'Hi'),
+    );
+    emojis.add(
+      EmojiModel(emoji: '😊', boxcolor: Color(0xacfa70), name: 'Happy'),
+    );
+    emojis.add(
+      EmojiModel(emoji: '😂', boxcolor: Color(0xacfa70), name: 'Funny'),
+    );
+    emojis.add(
+      EmojiModel(emoji: '🤣', boxcolor: Color(0xacfa70), name: 'LOL'),
+    );
+    emojis.add(
+      EmojiModel(emoji: '❤️', boxcolor: Color(0xacfa70), name: 'Like'),
+    );
+    emojis.add(
+      EmojiModel(emoji: '😍', boxcolor: Color(0xacfa70), name: 'Love'),
+    );
+
+    return emojis;
+  }
+}
